dc85d6474acf20a2651e85cbbf2e6a3b7739afce
trapedev <mvemtae5757@outlook.jp>
2023-06-12 11:50:14 +0900

    formtted

--- kazuate/src/main/java/com/kazuate/Main.java
+++ kazuate/src/main/java/com/kazuate/Main.java
@@ -15,20 +15,20 @@ public class Main
             System.out.println("こちらで予想して欲しい数（以下，ターゲット数）を生成しました．あなたが予想した数を入力してください．");
             int guess = sc.nextInt();
             if (guess == targetNumber) {
-                    System.out.println("当たり！！！！！！！！");
-                    sc.close();
-                    return;
-                } else if (guess < targetNumber) {
-                    System.out.println("予想した数はターゲット数より小さいです．");
-                    if (targetNumber - guess >= 20) {
-                        System.out.println("ターゲット数より20以上小さいです．");
-                    }
-                } else {
-                    System.out.println("予想した数はターゲット数より大きいです．");
-                    if (guess - targetNumber >= 20) {
-                        System.out.println("ターゲット数より20以上大きいです．");
-                    }
+                System.out.println("当たり！！！！！！！！");
+                sc.close();
+                return;
+            } else if (guess < targetNumber) {
+                System.out.println("予想した数はターゲット数より小さいです．");
+                if (targetNumber - guess >= 20) {
+                    System.out.println("ターゲット数より20以上小さいです．");
                 }
+            } else {
+                System.out.println("予想した数はターゲット数より大きいです．");
+                if (guess - targetNumber >= 20) {
+                    System.out.println("ターゲット数より20以上大きいです．");
+                }
+            }
         }
         System.out.println("5回の予想が終了しました．ターゲット数は " + targetNumber + " でした．お疲れ様でした．");
         sc.close();
