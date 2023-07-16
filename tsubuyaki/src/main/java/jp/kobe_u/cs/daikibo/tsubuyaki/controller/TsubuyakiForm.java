687e9c78979b3feb934c17f695f6277829a491bd
trapedev <mvemtae5757@outlook.jp>
2023-07-16 18:13:09 +0900

    init

---
+++ tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiForm.java
@@ -0,0 +1,9 @@
+package jp.kobe_u.cs.daikibo.tsubuyaki.controller;
+
+import lombok.Data;
+
+@Data
+public class TsubuyakiForm {
+    String name;
+    String comment;
+}
---
+++ tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/service/TsubuyakiService.java
@@ -0,0 +1,31 @@
+package jp.kobe_u.cs.daikibo.tsubuyaki.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jp.kobe_u.cs.daikibo.tsubuyaki.entity.Tsubuyaki;
+import jp.kobe_u.cs.daikibo.tsubuyaki.repository.TsubuyakiRepository;
+
+public class TsubuyakiService {
+    @Autowired
+    TsubuyakiRepository repo;
+
+    public Tsubuyaki postTsubuyaki(Tsubuyaki t) {
+        String name = t.getName();
+        if (name == null || name.length()==0) {
+            t.setName("名無しさん");
+        }
+        t.setCreatedAt(new Date());
+        return repo.save(t);
+    }
+
+    public List<Tsubuyaki> getAllTsubuyaki() {
+        Iterable<Tsubuyaki> found = repo.findAll();
+        ArrayList<Tsubuyaki> list = new ArrayList<>();
+        found.forEach(list::add);
+        return list;
+    }
+}
